afa922e40ba05ff8f73ee4f8815b97b45c38ff01
Jason Liang <jasonliang512@gmail.com>
2019-12-02 00:28:06 -0800

    hello world

--- main.c
+++ main.c
@@ -3,28 +3,69 @@
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+#define ASSET_DIRECTORY "../assets/"
 
-int main() {
+SDL_Window *window = NULL;
+SDL_Surface *screenSurface = NULL;
+SDL_Surface *hello = NULL;
+
+int init() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not initialize SDL_Init Error: %s\n", SDL_GetError());
-    return EXIT_FAILURE;
+    return 0;
   }
 
-  SDL_Window *window = SDL_CreateWindow(
-      "Conway's Game of Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-      SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  window = SDL_CreateWindow("Conway's Game of Life", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
   if (window == NULL) {
     printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+    return 0;
+  }
+
+  screenSurface = SDL_GetWindowSurface(window);
+
+  return 1;
+}
+
+int loadMedia() {
+  hello = SDL_LoadBMP(ASSET_DIRECTORY "hello.bmp");
+  if (hello == NULL) {
+    printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+    return 0;
+  }
+
+  return 1;
+}
+
+void cleanup() {
+  SDL_FreeSurface(hello);
+  hello = NULL;
+
+  SDL_DestroyWindow(window);
+  window = NULL;
+
+  SDL_Quit();
+}
+
+SDL_Surface *loadSurface(char *path) {
+  SDL_Surface *surface = SDL_LoadBMP(path);
+
+  if (surface == NULL) {
+    printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+  }
+
+  return surface;
+}
+
+int main() {
+  if (!init()) {
     return EXIT_FAILURE;
   }
 
-  SDL_Renderer *renderer =
-      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-
-  if (renderer == NULL) {
-    SDL_DestroyWindow(window);
-    printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+  if (!loadMedia()) {
+    return EXIT_FAILURE;
   }
 
   SDL_Event event;
@@ -39,13 +80,11 @@ int main() {
       }
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    SDL_BlitSurface(hello, NULL, screenSurface, NULL);
+    SDL_UpdateWindowSurface(window);
   }
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  cleanup();
 
   return 0;
 }
