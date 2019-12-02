3652e56df568a75c1b77d0f3ed87d0fc6b80396c
Jason Liang <jasonliang512@gmail.com>
2019-12-02 01:16:09 -0800

    drawing images

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 
 #define SCREEN_WIDTH 640
@@ -6,12 +7,12 @@
 #define ASSET_DIRECTORY "../assets/"
 
 SDL_Window *window = NULL;
-SDL_Surface *screenSurface = NULL;
-SDL_Surface *hello = NULL;
+SDL_Renderer *renderer = NULL;
+SDL_Texture *helloAgain = NULL;
 
 int init() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("SDL could not initialize SDL_Init Error: %s\n", SDL_GetError());
+    printf("SDL could not initialize. SDL_Init Error: %s\n", SDL_GetError());
     return 0;
   }
 
@@ -24,15 +25,48 @@ int init() {
     return 0;
   }
 
-  screenSurface = SDL_GetWindowSurface(window);
+  renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  if (renderer == NULL) {
+    printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+    return 0;
+  }
+
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+
+  if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+    printf("IMG_Init Error: %s\n", IMG_GetError());
+    return 0;
+  }
 
   return 1;
 }
 
+SDL_Texture *loadTexture(char *path) {
+  SDL_Surface *loadedSurface = IMG_Load(path);
+
+  if (loadedSurface == NULL) {
+    printf("IMG_Load Error: %s\n", IMG_GetError());
+    return NULL;
+  }
+
+  SDL_Texture *newTexture =
+      SDL_CreateTextureFromSurface(renderer, loadedSurface);
+
+  if (newTexture == NULL) {
+    printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+    return NULL;
+  }
+
+  SDL_FreeSurface(loadedSurface);
+  return newTexture;
+}
+
 int loadMedia() {
-  hello = SDL_LoadBMP(ASSET_DIRECTORY "hello.bmp");
-  if (hello == NULL) {
-    printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
+  helloAgain = loadTexture(ASSET_DIRECTORY "preview.png");
+
+  if (helloAgain == NULL) {
     return 0;
   }
 
@@ -40,23 +74,17 @@ int loadMedia() {
 }
 
 void cleanup() {
-  SDL_FreeSurface(hello);
-  hello = NULL;
+  SDL_DestroyTexture(helloAgain);
+  helloAgain = NULL;
+
+  SDL_DestroyRenderer(renderer);
+  renderer = NULL;
 
   SDL_DestroyWindow(window);
   window = NULL;
 
+  IMG_Quit();
   SDL_Quit();
-}
-
-SDL_Surface *loadSurface(char *path) {
-  SDL_Surface *surface = SDL_LoadBMP(path);
-
-  if (surface == NULL) {
-    printf("SDL_LoadBMP Error: %s\n", SDL_GetError());
-  }
-
-  return surface;
 }
 
 int main() {
@@ -80,8 +108,9 @@ int main() {
       }
     }
 
-    SDL_BlitSurface(hello, NULL, screenSurface, NULL);
-    SDL_UpdateWindowSurface(window);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, helloAgain, NULL, NULL);
+    SDL_RenderPresent(renderer);
   }
 
   cleanup();
